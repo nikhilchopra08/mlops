16770f28d8cc90270c4b1f091da23bbdf1c59ca3
Nikhil chopra <nikhil0080012005@gmail.com>
2026-03-08 18:52:27 +0530

    updated the code for evaluation based on the happiness

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-
 import pickle
 import json
 
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score, recall_score, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 
 clf = pickle.load(open('model.pkl','rb'))
 test_data = pd.read_csv('./data/features/test_bow.csv')
@@ -14,20 +12,22 @@ X_test = test_data.iloc[:,0:-1].values
 y_test = test_data.iloc[:,-1].values
 
 y_pred = clf.predict(X_test)
-y_pred_proba = clf.predict_proba(X_test)[:, 1]
 
-# Calculate evaluation metrics
+pos_label = "happiness"
+pos_index = list(clf.classes_).index(pos_label)
+y_pred_proba = clf.predict_proba(X_test)[:, pos_index]
+
 accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, pos_label=pos_label)
+recall = recall_score(y_test, y_pred, pos_label=pos_label)
 auc = roc_auc_score(y_test, y_pred_proba)
 
-metrics_dict={
-    'accuracy':accuracy,
-    'precision':precision,
-    'recall':recall,
-    'auc':auc
+metrics_dict = {
+    "accuracy": accuracy,
+    "precision": precision,
+    "recall": recall,
+    "auc": auc
 }
 
-with open('metrics.json', 'w') as file:
+with open("metrics.json", "w") as file:
     json.dump(metrics_dict, file, indent=4)
